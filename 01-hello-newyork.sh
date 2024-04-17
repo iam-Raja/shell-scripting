1a6f6eb5c8f0f4bdc886ad2e3d09ebd0dd5afd41
iam-raja <rajapeta45@gmail.com>
2024-04-17 10:39:18 +0530

    demo

--- 01-hello-newyork.sh
+++ 01-hello-newyork.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+#declaring variable
 x=$1
 y=$2
+
+#refering variable
 echo "hi, hello newyork"
 echo "$x:: hi $y"
 echo "$y:: hello $x"
---
+++ 03.variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter username::"
+# -s to hide
+read USERNAME
+
+echo "Enter password::"
+
+read PASSWORD
+
+echo "username:$USERNAME and password:$PASSWORD"
