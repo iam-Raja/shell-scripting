5d17655ad767f243e90c263dbf7a5080ecfb3fb2
iam-raja <rajapeta45@gmail.com>
2024-04-17 08:37:18 +0530

    demo

--- 01-hello-newyork.sh
+++ 01-hello-newyork.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+x=raja
+y=sagar
 echo "hi, hello newyork"
+echo "$x:: hi $y"
+echo "$y:: hello $x"
+echo "$x:: do u have gf"
+echo "$y:: no"
+echo "$x:: why?"
+echo "$y:: my mistake don't have the fire"
