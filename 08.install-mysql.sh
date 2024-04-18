5acd210c09fd053693c9d4f11a39394e7db76868
iam-raja <rajapeta45@gmail.com>
2024-04-18 11:20:39 +0530

    demo-commit

--- 08.install-mysql.sh
+++ 08.install-mysql.sh
@@ -2,9 +2,10 @@
 USERID=$(id -u)
 dnf install mysql -y
 echo "mysql installed successfully"
+dnf install git -y
 
 if [$USERID -eq 0]
 then
-echo "you are super user"
+echo "you are super user, installed"
 else
 echo "switch to root....proceed....."
