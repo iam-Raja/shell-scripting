1fca15c155b2db2cc5ee8704a283ec2faf5aaeea
iam-raja <rajapeta45@gmail.com>
2024-04-18 10:50:09 +0530

    demo-commit

--- 07.conditionds.sh
+++ 07.conditionds.sh
@@ -2,12 +2,9 @@
 
 x=$1
 
-if [ $x -eq 100 ]
-
+if [ $x -eq 45 ]
 then
-    echo "given is $x is=100"
-
+echo "given is $x is=45"
 else
-    echo "given no is $x is!=100"
-
+echo "given no is $x is!=45"
 fi
