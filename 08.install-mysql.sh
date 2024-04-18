5856004e61ccb32fbe3a95e2c581f938dccb2431
iam-raja <rajapeta45@gmail.com>
2024-04-18 11:26:51 +0530

    demo-commit

--- 08.install-mysql.sh
+++ 08.install-mysql.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 USERID=$(id -u)
+
 dnf install mysql -y
+if [ $? -ne 0]
+then
+echo "mysql install failed"
+exit 1
+else
 echo "mysql installed successfully"
+fi
+
 dnf install git -y
+
+if [ $? -ne 0]
+then
+echo "git install failed"
+exit 1
+else
+echo "git installed successfully"
+fi
 
 if [$USERID -eq 0]
 then
