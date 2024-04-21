cca67e6f70d79a133677590288089850f77db253
iam-raja <rajapeta45@gmail.com>
2024-04-21 15:28:00 +0530

    demo-commit

--- 05.datatypes.sh
+++ 05.datatypes.sh
@@ -2,8 +2,9 @@
 x=$1
 y=$2
 
-#sum=$(($x+$y))
-add=$(($x+$y))
-echo ""value of $1 abd $2 is ::$add"
+sum=$(($x+$y))
+#add=$(($x+$y))
 
-#echo "value of $1 and $2 is:: $sum"
+#echo ""value is $1 abd $2 is ::$add"
+
+echo "value of $1 and $2 is:: $sum"
