3c51f5c232f3d6adf37e98189194220bdf2af9b2
iam-raja <rajapeta45@gmail.com>
2024-04-21 16:08:39 +0530

    demo-commit

--- 04.array.sh
+++ 04.array.sh
@@ -5,4 +5,3 @@ Team=("rohit" "sky" "bumrah")
 echo "first name is::${Team[0]}"
 echo "first name is::${Team[1]}"
 echo "first name is::${Team[@]}"
-echo "all is well :: $Team"
--- 07.conditionds.sh
+++ 07.conditionds.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-x=$1
+: ' x=$1
 
 if [ $x -eq 45 ]
 then
 echo "given is $x is=45"
 else
 echo "given no is $x is!=45"
-fi
+fi '
+
+no1=$1
+no2=$2
+
+if [$no -eq $no2]
+then echo " both $no1 and $no2 are eua1"
+else echo " given $no1 and $no2 are not eua1"
+if
+
 
 # -gt, -lt, -eq, -ge, -le
