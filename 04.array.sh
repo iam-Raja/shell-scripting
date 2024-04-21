547a5a3b799ac6a272ba9ea6cb73492749f0dbd2
iam-raja <rajapeta45@gmail.com>
2024-04-21 15:50:28 +0530

    demo-commit

--- 04.array.sh
+++ 04.array.sh
@@ -5,4 +5,4 @@ Team=("rohit" "sky" "bumrah")
 echo "first name is::${Team[0]}"
 echo "first name is::${Team[1]}"
 echo "first name is::${Team[@]}"
-echo "all is well :: $Team[1]"
+echo "all is well :: $Team 1"
