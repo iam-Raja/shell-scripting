fbaa32dad133b6d51343c386a3ae16da025c1b86
iam-raja <rajapeta45@gmail.com>
2024-04-22 08:19:38 +0530

    demo-commit

--- exepnse-automation.sh/my-sql.sh
+++ exepnse-automation.sh/my-sql.sh
@@ -3,6 +3,17 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 
+validate (){ 
+    if [ $1 -ne 0 ]
+    then echo " $2 ...failed"
+    exit 1
+    else echo " $2 ...success"
+    fi
+}
+
+dnf install mysql-server -y 
+validate $? " installing mysql"
+
 if [ $USERID -ne 0 ]
 then echo -e " $R switch to root user $N "
 exit 1
