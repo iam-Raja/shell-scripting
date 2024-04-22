a67d0d8a568ead5279c627292102952726724f12
iam-raja <rajapeta45@gmail.com>
2024-04-22 08:35:56 +0530

    demo-commit

--- exepnse-automation.sh/my-sql.sh
+++ exepnse-automation.sh/my-sql.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 USERID=$(id -u)
+Timestamp=$(date +%F-%H-%M-%s)
+script=$(echo $0|cut -d "." -f1)
+log_file=/tmp/$Timestamp-$script.log
 R="\e[31m"
 G="\e[32m"
 
@@ -10,13 +13,20 @@ else echo -e " $G u r rot user $N "
 fi
 validate (){ 
     if [ $1 -ne 0 ]
-    then echo " $2 ...failed"
+    then echo -e " $2 ...$R failed $N "
     exit 1
-    else echo " $2 ...success"
+    else echo -e " $2 ...$G success $N "
     fi
 }
 
-dnf install mysql-server -y 
+dnf install mysql-server -y &>$log_file
 validate $? " installing mysql"
+systemctl enable mysqld &>$log_file
+validate $? " enabling mysql"
+systemctl start mysqld &>$log_file
+validate $? " starting mysql"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>$log_file
+validate $? " setting usid&pwd mysql"
+
 
 
