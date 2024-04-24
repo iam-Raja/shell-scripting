986b66db593d10e2125cb152a8c1f41f46fe1740
iam-raja <rajapeta45@gmail.com>
2024-04-24 11:07:25 +0530

    demo-commit

--- 12.script-calling.sh
+++ 12.script-calling.sh
@@ -5,5 +5,5 @@ course="devops from current scriptr 12"
 echo "Before calling other script, calling::$course"
 echo "PID of current script::$$"
 
-souce ./13.calling-demo-script.sh
+source ./13.calling-demo-script.sh
 echo "after calling other script, calling: $course"
