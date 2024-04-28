2d23df4ff4bf3765cd721eb3973f5dad60f4e69a
iam-raja <rajapeta45@gmail.com>
2024-04-28 10:23:10 +0530

    sample-commit

--- 14.set-e_x_o.sh
+++ 14.set-e_x_o.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 USERID=$(id -u)
-
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 set -x #debug mode
 set -e #exit when script error
 
-if [$USERID -ne 0]
-then echo "Switch to super user"
-else echo "U R Super User"
+if [ $USERID -ne 0 ]
+then echo -e "$R Switch to super user $N"
+else echo -e " $G U R Super User $N"
 fi
 
 dnf install git -y
 dnf install mysqll -y
-echo "installed git & Mysql"
+echo -e " $G installed git & Mysql $N"
 
