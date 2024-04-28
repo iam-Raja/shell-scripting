f0362f17ad5f85c2501635951271d6730a7ef20b
iam-raja <rajapeta45@gmail.com>
2024-04-28 10:40:54 +0530

    sample-commit

--- 14.set-e_x_o.sh
+++ 14.set-e_x_o.sh
@@ -5,6 +5,11 @@ G="\e[32m"
 N="\e[0m"
 set -x #debug mode
 set -e #exit when script error
+error(){
+    echo "failed at $1 : $msg"
+}
+
+trap 'error ${LINENO} "$BASH_COMMAND"' ERR
 
 if [ $USERID -ne 0 ]
 then echo -e "$R Switch to super user $N"
@@ -12,7 +17,7 @@ exit 1
 else echo -e " $G U R Super User $N"
 fi
 
-dnf install git -y
-dnf install mysqll -y
+dnf install gitt -y
+dnf install mysq -y
 echo -e " $G installed git & Mysql $N"
 
